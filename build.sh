24bb008b43537454d96b87dfacf61e6fabf24515
Hakan <hknhzl@gmail.com>
2024-02-15 11:26:03 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,8 +25,6 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
 source build/envsetup.sh && \
 
 # Lunch configuration
-riseup tissot ;\
-purge ;\
 lunch rising_tissot-userdebug ;\
 
 croot ;\
