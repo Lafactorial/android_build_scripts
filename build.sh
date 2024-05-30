c4366a849b969089028a6272311f1b7fab74dce4
Hakan <hknhzl@gmail.com>
2024-05-30 18:04:13 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,10 +16,10 @@ git clone https://github.com/Lafactorial/local_manifest --depth 1 -b sweet2-alph
 
 # Sync the repositories
 /opt/crave/resync.sh && \ 
-repo forall -c "git lfs install && git lfs pull && git lfs checkout";
+repo forall -c "git lfs install && git lfs pull && git lfs checkout" ;\
 
 # Signing
-export SIGNING_PREFERENCE=true ;\
+export SIGNING_PREFERENCE=true
 curl https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/signing/scripts/signing-script.sh | bash ;\
 
 # Set up build environment
