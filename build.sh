1fd2e84f33f0629fe0c5b8f3c0c7ac43a2ff60ef
Hakan <hknhzl@gmail.com>
2024-05-30 18:28:54 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,8 +18,7 @@ git clone https://github.com/Lafactorial/local_manifest --depth 1 -b sweet2-alph
 /opt/crave/resync.sh && \ 
 
 # lfs stuff
-cd vendor/gms && git lfs fetch && git lfs install && git lfs checkout && cd ../.. ;\
-cd external/chromium-webview/prebuilt/arm64 && git lfs fetch && git lfs install && git lfs checkout && cd ../.. ;\
+repo forall -c "git lfs install && git lfs pull && git lfs checkout" ;\
 
 # Signing
 export SIGNING_PREFERENCE=true ;\
