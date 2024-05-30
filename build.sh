9161dc3ebed5e82e66126454e002acd6b9ddefdd
Hakan <hknhzl@gmail.com>
2024-05-30 12:12:07 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ git clone https://github.com/Lafactorial/local_manifest --depth 1 -b sweet2-alph
 
 # Sync the repositories
 /opt/crave/resync.sh && \ 
-repo forall -c 'git lfs install && git lfs pull && git lfs checkout' ;\
+repo forall -c "git lfs install && git lfs pull && git lfs checkout" ;\
 
 # Signing
 curl https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/signing/scripts/signing-script.sh | bash; \
