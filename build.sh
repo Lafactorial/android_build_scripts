a44ed3ead994056a89cad997677ef45432b27ad4
Hakan <hknhzl@gmail.com>
2024-05-30 15:41:47 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -36,3 +36,20 @@ make bacon ; \
 
 # Print SHA256
 # sha256sum out/target/product/*/*.zip"
+
+# Clean up
+# rm -rf tissot/*
+
+
+
+# Pull generated zip files
+# crave pull out/target/product/*/*.zip
+
+# Pull generated img files
+# crave pull out/target/product/*/*.img
+
+# Upload zips to Telegram
+# telegram-upload --to sdreleases tissot/*.zip
+
+#Upload to Github Releases
+#curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
