c346f98cf801309776b76e9fc7dd82fde9be54ce
Hakan <hknhzl@gmail.com>
2024-06-25 08:12:01 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ git clone https://github.com/Lafactorial/local_manifest --depth 1 -b sweet2-alph
 source build/envsetup.sh && \
 
 # Lunch configuration
-breakfast sweet2 ;\
+breakfast sweet2 userdebug ;\
 
 croot ;\
 repo forall -c 'git lfs install && git lfs pull && git lfs checkout' ;\
